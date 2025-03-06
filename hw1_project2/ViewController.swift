0a95d7413e1648b55f7525aa220df663e8eb0e0f
«StasyStar» <Anstar1991@gmail.com>
2025-03-06 12:34:03 +0300

    Initial Commit

---
+++ hw1_project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1_project2
+//
+//  Created by Anastasia on 06.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
